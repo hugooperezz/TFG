1be1878d196087c568252021f5d010bc31c7940c
hugooperezz <hugo.perez.redondo@colegiolitterator.com>
2025-05-19 13:21:05 +0200

    Tailwind añadido y componente navbar actualizado

--- TFG/src/app/components/shared/navbar/navbar.component.ts
+++ TFG/src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isMenuOpen: boolean = false;
+
   constructor(private router: Router) {}
+
   buscarDiscoteca(termino: string) {
     this.router.navigate(['/buscar', termino]);
   }
