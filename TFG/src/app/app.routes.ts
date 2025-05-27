dc24a2233c8d69838f302126f86b11402ec2e058
hugooperezz <hugo.perez.redondo@colegiolitterator.com>
2025-05-27 21:38:31 +0200

    font login y register añadido

--- TFG/src/app/app.routes.ts
+++ TFG/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { AboutComponent } from './components/about/about.component';
 import { DiscotecasComponent } from './components/discotecas/discotecas.component';
 import { DiscotecaComponent } from './components/discoteca/discoteca.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegistroComponent } from './components/auth/registro/registro.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -12,5 +14,7 @@ export const routes: Routes = [
   { path: 'discotecas', component: DiscotecasComponent },
   { path: 'discoteca/:id', component: DiscotecaComponent },
   { path: 'buscar/:termino', component: BuscadorComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
